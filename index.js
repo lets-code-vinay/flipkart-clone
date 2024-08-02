150951351e73a879acd060dfe28468313021154c
vinay <vinay.shermauryan@gmail.com>
2024-08-02 10:57:03 +0530

    project folder setup

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<h1>Hi..From flipkart-clone</h1>);
